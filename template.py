3dc28e1d0b15bfa6ee578170d7af6a2742c4c2a6
Kristen Schneider <kristen@Kristens-MacBook-Pro.local>
2019-07-28 17:27:25 -0600

    added looping menu option

--- template.py
+++ template.py
@@ -7,7 +7,12 @@ def play_game():
     	while user_entry != 1 and user_entry != 2: user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
     	while user_entry == 1:
 
-	## YOUR CODE BELOW
+		## YOUR CODE HERE
 
+        	## Q1, example
+        	answer1 = input('Do you like coding?\n1.Yes\n2.No\n\nYour Answer: ')
+		
 
+		## REPEAT MENU ITEM
+        	user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
 play_game()
