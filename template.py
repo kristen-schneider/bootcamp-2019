3c6fc07aeaeaea9620ee1dc64a4fe2b9a57f476f
Kristen Schneider <kristen@Kristens-MacBook-Pro.local>
2019-07-28 17:53:27 -0600

    added step 1 in comments

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ def play_game():
     	while user_entry != 1 and user_entry != 2: user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
     	while user_entry == 1:
 
-		## YOUR CODE HERE
+		## STEP 1 HERE
 
         	## Q1, example
         	answer1 = input('Do you like coding?\n1. Yes\n2. No\n\nYour Answer: ')
