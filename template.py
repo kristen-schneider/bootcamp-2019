b7c04d1d76a550f9f36e17e5d2a93df7b97da483
Kristen Schneider <kristen@Kristens-MacBook-Pro.local>
2019-07-28 17:21:06 -0600

    added template to import sys and set up menu option

---
+++ template.py
@@ -0,0 +1,13 @@
+import sys
+  
+def play_game():
+    
+	## START GAME
+    	user_entry = 0
+    	while user_entry != 1 and user_entry != 2: user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
+    	while user_entry == 1:
+
+	## YOUR CODE BELOW
+
+
+play_game()
