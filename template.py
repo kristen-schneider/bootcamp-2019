70cfcc3d53153f22fb4f67f0bc0a032ab9d42a61
Kristen Schneider <kristen@Kristens-MacBook-Pro.local>
2019-08-08 14:22:23 -0500

    fixed tabs

--- template.py
+++ template.py
@@ -2,14 +2,13 @@ import sys
 
 ## Function to play friendship algorithm game  
 def play_game():
-    
 	## START GAME
-	
+
 	# initialize the user input to 0
-    	user_entry = 0
+	user_entry = 0
 	# ask the user to make their own input (1 to play or 2 to exit):
-    	while user_entry != 1 and user_entry != 2: user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
-    	# if the user selects 1, they want to play! Ask them questions and wait for their answers.
+	while user_entry != 1 and user_entry != 2: user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
+	# if the user selects 1, they want to play! Ask them questions and wait for their answers.
 	while user_entry == 1:
 		user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
 		## STEP 1 HERE
@@ -25,4 +24,4 @@ def play_game():
 
 	
 ## Function call to play friendship algorithm game
-play_game()	
+play_game()
