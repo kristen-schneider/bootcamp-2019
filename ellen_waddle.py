69c1acae71ce87c6fe46f9212914c637b0b5d52b
Ellen Waddle <ellenwaddle@cu-biot-9-10.203.161.163.int.colorado.edu>
2019-08-21 10:00:19 -0600

    added my python file

---
+++ ellen_waddle.py
@@ -0,0 +1,26 @@
+import sys
+
+## Function to play friendship algorithm game  
+def play_game():
+	## START GAME
+
+	# initialize the user input to 0
+	user_entry = 0
+	# ask the user to make their own input (1 to play or 2 to exit):
+	while user_entry != 1 and user_entry != 2: user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
+	# if the user selects 1, they want to play! Ask them questions and wait for their answers.
+	while user_entry == 1:
+		## STEP 1 HERE
+		
+		## STEP 2&3 HERE
+	
+		## STEP 4 HERE
+	
+		
+		user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
+	
+
+
+	
+## Function call to play friendship algorithm game
+play_game()
