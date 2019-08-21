a73c5860fe05042afa547cb62e5a04d00122f0b4
Peter Innes <peter.innes@colorado.edu>
2019-08-21 11:24:08 -0600

    changing structure

--- peter_innes.py
+++ peter_innes.py
@@ -1,27 +1,35 @@
 import sys
 
+#blah
+
 def play_game():
-    ## 1, 2, 3, GO!
-    user_entry=0
-    while user_entry != 1 and user_entry != 2: user_entry = input('Halp I need friends do you want to take my quiz?\n1. Yes\n2. No, sorry :(\nChoose 1 or 2: ')
-    if user_entry == 2: print 'goodbye'
-    while user_entry != 2:
-
-        total_score = 0
-
-        #QUESTION 1
-        ans1 = input('Pick your favorite coffee drink\n1. Cortado\n2. A damn fine cup of coffee \n3. Almond milk latte\n4. I prefer tea\n\nYour answer: ')
-        p1 = give_points(ans1)
-        while p1 == -1:
-            ans1 = input('INVALID INPUT. plz try again.\n\nYour answer: ')
-            p1 = give_points(ans1)
-        total_score += p1
-        
-        #CALCULATE FRIENDSHIP
-        calculate_friendship(total_score)
-        
-        # REPEAT MENU ITEM
-        user_entry = input('Select Option!\n1. Play again\n2. Exit Game\n\nYour Selection: ')
+	user_entry=0
+	while user_entry == 0:
+		user_entry = input('1:yes\n2:no\n\n')
+	if user_entry == 2: 
+		print('goodbye')
+	while user_entry == 1:
+		total_score = 0
+	
+		#QUESTIONS
+		ans1 = input('Pick your favorite coffee drink\n1. Cortado\n2. A damn fine cup of coffee \n3. Almond milk latte\n4. I prefer tea\n\nYour answer: ')
+		ans2 = input('What dinosaur as pet?\n1. duck-thing\n2. ptero\n3. stego\n4. parrot\n\nYour answer: ')       
+		ans3 = input('Fill in the blank: grad school fills me with ______\n1. Knowledge\n2. I am empty inside\n3. Curiosity and wonder\n4. existensial dread\n\nYour answer: ')
+		ans4 = input('Favorite life hack:\n1. napping\n2. Telling your mom you love her\3. Netflix\4. Pagan rituals\n\nYour answer: ')
+		
+		answers = [ans1,ans2,ans3,ans4]
+		for ans in answers: 
+			points = give_points(ans)
+			while points == -1:
+				ans = input('INVALID INPUT. plz try again.\n\nYour answer: ')
+				points = give_points(ans1)
+				total_score += points
+        	
+		#CALCULATE FRIENDSHIP
+		calculate_friendship(total_score)
+        	
+        	# REPEAT MENU ITEM
+		user_entry = input('Select Option!\n1. Play again\n2. Exit Game\n\nYour Selection: ')
 
 def give_points(answer):
     q_points = 0
@@ -34,7 +42,7 @@ def give_points(answer):
     return q_points
 
 def calculate_friendship(points):
-    if points >= 5: print 'let us be friends\n'
+    if points >= 5: print('let us be friends\n')
     elif points < 5: ''
 
 play_game()
