244f0fc85bbbe86cd29ba0b21846c703fe8b56c3
Adam Dziulko <adamdziulko@gmail.com>
2019-08-21 15:52:52 -0600

    updated version

--- Adam_Dziulko.py
+++ Adam_Dziulko.py
@@ -6,21 +6,36 @@ def play_game():
 
 	# initialize the user input to 0
 	user_entry = 0
+	user_entry = int(user_entry)
 	# ask the user to make their own input (1 to play or 2 to exit):
-	while user_entry != 1 and user_entry != 2: user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
+	while user_entry != 1 and user_entry != 2: user_entry = int(input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: '))
 	# if the user selects 1, they want to play! Ask them questions and wait for their answers.
-	while user_entry == 1:
+	while int(user_entry) == 1:
 		## STEP 1 HERE
 		total_points = 0
-		answer_one = input("Do you like coding? \n. Yes\n2. No")		
+		answer_one = int(input("Do you want to be BFF's?\n1. Yes\n2. No\n\nYour Selection: "))		
 		## STEP 2&3 HERE
-		if answer_one == 1: total_points += 5
-		else: total_points += 0	
-		## STEP 4 HERE
+		if answer_one == 1: input("Congratz! We are now BFF's!\nPress any key to continue")
+		elif answer_one == 2: answer_one_two = int(input("Are you sure?\n1.Yes, Def\n2.Nah, lets be BFF's\n\nYour Selection: "))
+		if answer_one_two == 1 :
+			input("Your loss") 
+			break
+		elif answer_one_two == 2 : input("Congratz! We are now BFF's!\nPress any key to continue")
+		## q4
+		answer_two = int(input("Jk, we should get to know each other first. Are you a good person?\n1.Yes\n2.No\n\nYour selection: "))
+		if answer_two == 1 : total_points =+ 5
+		elif answer_two == 2 : total_points =+ 1
+		else: total_points += 0
+		#q3
+		answer_three = int(input("What is your favorite color?\n 1.Blue\n 2.Blue\n 3.Green....JK, Blue\n\n Your Selection: "))
+		if answer_three  == 1: total_points += 5
+		elif answer_three  == 2: total_points += 5
+		else: total_points += 0
+		#print statement
 		print(total_points)	
 		
 		user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
-	
+		user_entry = int(user_entry)
 
 
 	
