c2f9c41c216e93a72a9bb02c3fec93bd49628b67
kbubar <kate.bubar@colorado.edu>
2019-08-21 10:23:22 -0600

    completed step 1

--- kate_bubar.py
+++ kate_bubar.py
@@ -11,7 +11,8 @@ def play_game():
 	# if the user selects 1, they want to play! Ask them questions and wait for their answers.
 	while user_entry == 1:
 		## STEP 1 HERE
-		
+	        total_points = 0
+                answer_one = input("Which TV Show is better?\n1. Friends\n 2. How I Met Your Mother\n 3. I don't think either") 
 		## STEP 2&3 HERE
 	
 		## STEP 4 HERE
