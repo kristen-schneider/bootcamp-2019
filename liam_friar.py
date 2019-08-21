857d7640f72413fcdecc288f62a9a427727f2899
liamfriar <liamfriar@gmail.com>
2019-08-21 15:33:45 -0600

    final final

--- liam_friar.py
+++ liam_friar.py
@@ -14,16 +14,22 @@ def play_game():
                 total_points = 0
                 answer_one = input ("Have you ever kissed a squirrel?!\n1. Yes\n2. No\n\nYour Selection: ")
                 ## STEP 2&3 HERE
-                if answer_one == "1":
+                if int(answer_one) == 1:
                         total_points += 5
                 else:
                         total_points -= 5
+		
+		answer_two = input ("Can you eat a rubick's cube in under 30 seconds?\n1. Yes\n2. No\n\nYour Selection: ")
+		if int(answer_two) == 1:
+			total_points += 5
+		else:
+			total_points -= 5
 
 
                 ## STEP 4 HERE
-                        print "We are {} percent best friends".format(total_points)
+                print "We are {}0 percent best friends".format(total_points)
 
-                user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
+                user_entry = input('Want to play again?!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
 
 
 
